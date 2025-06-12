b822fe163d07b0102d12cca835cf3ccb44f84b43
IkeyBenz <ikey.benz@gmail.com>
2025-06-12 18:54:37 -0400

    get rid of annoying redlines

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "src/generated/**",
+      "**/*.js"
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,13 +1,1 @@
 import '@testing-library/jest-dom'
-
-// Extend Jest's expect with testing-library matchers
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeInTheDocument(): R
-      toHaveValue(value: string | number | string[]): R
-      toHaveAttribute(attr: string, value?: string): R
-      toBeInvalid(): R
-    }
-  }
-} 
