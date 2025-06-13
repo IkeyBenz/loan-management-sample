641a74b3060e3f7300760a5be89050ee10ab1f1e
IkeyBenz <ikey.benz@gmail.com>
2025-06-13 13:00:44 -0400

    fix bug that causes loan list to not display all loans

--- src/app/loans/page.tsx
+++ src/app/loans/page.tsx
@@ -1,6 +1,10 @@
 import { getLoanList, LoanWithStatus } from "@/app/actions/loans/list";
 import LoanList from "@/components/Loans/LoanList";
 
+// Ensure we're always fetching fresh data when rendering this page.
+// Prevent Next.js from statically rendering at build time.
+export const dynamic = "force-dynamic";
+
 export default async function LoansListPage() {
   // Fetch loans from the new action
   const loans: LoanWithStatus[] = await getLoanList();
